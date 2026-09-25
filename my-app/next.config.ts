55b098c1c474ddd3da4ee66e7134874aef305986
Sindhumind <sindhujap@mindfiresolutions.com>
2026-09-25 21:45:34 +0530

    updated ghe next config to ngrok

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -4,11 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
+  protocol: "http",
+  hostname: "localhost",
+  port: "1337",
+  pathname: "/uploads/**",
+},
+{
+  protocol: "https",
+  hostname: "https://repose-bankable-selector.ngrok-free.dev",
+  pathname: "/uploads/**",
+},
     ],
   },
 };
